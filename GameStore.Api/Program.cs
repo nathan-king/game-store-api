99912a4e55719d0736b9eebd031138f812e71e27
Nathan King <hello@nathanking.io>
2026-01-19 11:41:04 +1100

    Add Sqlite database

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,11 +1,15 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
+builder.AddGameStoreDb();
 
 WebApplication app = builder.Build();
 
 app.MapGamesEndpoints();
+
+app.MigrateDb();
 
 app.Run();
